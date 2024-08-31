d38af0c75ae01b4f5bfb30c8d2eea9e5249f88d8
facusada <facundosadava1@gmail.com>
2024-08-31 02:11:31 -0300

    Add passport config and initialize. It hasnt beet implemented yet

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes.js';
+import passport from 'passport';
+import passportConfig from './config/passport.js';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+passportConfig(passport);
+app.use(passport.initialize());
+
 app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 8000;
